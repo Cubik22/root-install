134b9b0e7c6ae98184c6e1ab98bc96cf1af34b5c
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-19 15:30:58 +0200

    Add void-chroot-install

---
+++ void-chroot-install.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# create ignorepkg file
+xbpsconf=/mnt/etc/xbps.d
+ignorefile="$xbpsconf"/ignorepkg.conf
+
+# recreate file if already existent
+mkdir -p $xbpsconf
+rm -f $ignorefile
+touch $ignorefile
+
+# set packages to be ignored as dependecy
+echo "ignorepkg=sudo" >> $ignorefile
+echo "ignorepkg=nvi" >> $ignorefile
+echo "ignorepkg=linux-firmware-amd" >> $ignorefile
+echo "ignorepkg=linux-firmware-nvidia" >> $ignorefile
+
+# install base packages, y flag for answering always yes
+#XBPS_ARCH=x86_64-musl xbps-install -S -r /mnt -R "https://alpha.de.repo.voidlinux.org/current/musl" base-files ncurses coreutils findutils diffutils libgcc dash bash grep gzip file sed gawk less util-linux which tar man-pages mdocml shadow e3fsprogs dosfstools procps-ng tzdata pciutils usbutils iana-etc openssh kbd iproute2 iputils iw iwd xbps neovim opendoas wifi-firmware void-artwork traceroute ethtool kmod acpid eudev runit-void removed-packages musl linux linux-headers
+
+#XBPS_ARCH=x86_64-musl xbps-install -S -r /mnt -R "https://alpha.de.repo.voidlinux.org/current/musl"
+#while read pkg; do
+#	XBPS_ARCH=x86_64-musl xbps-install -S -y -r /mnt -R "https://alpha.de.repo.voidlinux.org/current/musl" $pkg
+#done <<EOT
+#$(cat packages.txt)
+#EOT
+
+# install basic packages
+echo | XBPS_ARCH=x86_64-musl xbps-install -S -y -r /mnt -R "https://alpha.de.repo.voidlinux.org/current/musl" base-system linux-headers cryptsetup opendoas neovim iwd
+
+# in order to have network
+cp /etc/resolv.conf /mnt/etc/resolv.conf
+cp /etc/hosts /mnt/etc/hosts
+
+# mount pseudo-filesystems
+mount -t proc none /mnt/proc
+mount -t sysfs none /mnt/sys
+mount --rbind /dev /mnt/dev
+mount --rbind /run /mnt/run
+
+# set hostname
+echo "voidlollo" > /mnt/etc/hostname
+
+# set options in /etc/rc.conf
+echo >> /mnt/etc/rc.conf
+echo "HARDWARECLOCK=\"UTC\"" >> /mnt/etc/rc.conf
+echo "KEYMAP=\"us\"" >> /mnt/etc/rc.conf
+echo "TTYS=3" >> /mnt/etc/rc.conf
+echo "CGROUP_MODE=hybrid" >> /mnt/etc/rc.conf
+
+# create fstab file from the mounted system
+# in the chroot manually configure it
+cp /mnt/proc/mounts /mnt/etc/fstab
+
+# chroot into the new installation
+PS1='(chroot) # ' chroot /mnt/ /bin/bash
+
+# Todo in chroot
+
+# link doas to sudo
+#ln -s /usr/bin/doas /usr/bin/sudo
+
+# set timezone
+#ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
