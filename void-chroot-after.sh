87b460a2e78f1727061cd84a058a5537b8c8d05b
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-25 01:16:32 +0200

    Update void-chroot-after.sh

--- void-chroot-after.sh
+++ void-chroot-after.sh
@@ -12,7 +12,7 @@ echo "$hostname" > /etc/hostname
 
 # set hosts
 echo -e "127.0.0.1\tlocalhost" > /etc/hosts
-echo -e "::1\tlocalhost" >> /etc/hosts
+echo -e "::1\t\tlocalhost" >> /etc/hosts
 echo -e "127.0.1.1\t${hostname}.localdomain\t${hostname}" >> /etc/hosts
 
 # set options in /etc/rc.conf (already done in root_install.sh)
