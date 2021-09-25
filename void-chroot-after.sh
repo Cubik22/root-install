c4fca0a86479fec20437434e0ec60614c9b78467
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-26 00:41:48 +0200

    Update void-chroot-after.sh

--- void-chroot-after.sh
+++ void-chroot-after.sh
@@ -77,9 +77,10 @@ chmod u+x /etc/cron.weekly/fstrim
 
 # set timezone
 # if BIOS/UEFI clock is already set to the correct time use UTC
-#ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+# if using OpenNTPD set the correct zone
+ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
 #ln -sf /usr/share/zoneinfo/Etc/GMT+2 /etc/localtime
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+#ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 # link services
 ln -s /etc/sv/dbus /etc/runit/runsvdir/default/
