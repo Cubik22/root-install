0ccd72a067b4e535133b4f6a453320cf89e9cf8d
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-25 18:49:47 +0200

    Update root_install.sh

--- root_install.sh
+++ root_install.sh
@@ -4,27 +4,27 @@
 root_install="$(pwd)"/files
 
 # adding global environment variables
-cat "${root_install}"/environment >> /etc/
+cat "${root_install}"/environment >> /etc/environment
 
 # setting locales
-cat "${root_install}"/locale.conf > /etc/
+cat "${root_install}"/locale.conf > /etc/locale.conf
 
 # rc initialization file
-cat "${root_install}"/rc.conf > /etc/
+cat "${root_install}"/rc.conf > /etc/rc.conf
 
 # grub config file generator
-cat "${root_install}"/grub > /etc/default/
+cat "${root_install}"/grub > /etc/default/grub
 
 # acpid handler
-cat "${root_install}"/handler.sh > /etc/acpi/
+cat "${root_install}"/handler.sh > /etc/acpi/handler.sh
 
 mkdir -p /etc/bash/bashrc.d
 
 # bash completion script
-cat "${root_install}"/bash_completion.sh > /etc/bash/bashrc.d/
+cat "${root_install}"/bash_completion.sh > /etc/bash/bashrc.d/bash_completion.sh
 
 # bash complete alias
-cat "${root_install}"/complete_alias > /usr/share/bash-completion/
+cat "${root_install}"/complete_alias > /usr/share/bash-completion/complete_alias
 
 mkdir -p /etc/iwd
 
