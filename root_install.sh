b8b8127f9f8aa16fd2f1418af896e5db0636c31d
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-26 17:59:00 +0200

    Update root_install.sh

--- root_install.sh
+++ root_install.sh
@@ -27,7 +27,7 @@ cat "${root_install}"/custom.conf > /etc/dracut.conf.d/custom.conf
 
 # ls colors (done at the user level)
 # remember to run eval "$(dircolors /etc/DIR_COLORS)"
-#cat "{root_install}"/DIR_COLORS > /etc/DIR_COLORS
+cat "${root_install}"/DIR_COLORS > /etc/DIR_COLORS
 
 # bash completion script
 mkdir -p /etc/bash/bashrc.d
