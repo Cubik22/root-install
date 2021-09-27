3e441f5f39cf90a2e8d48aa4808bb4410693c471
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-27 21:53:34 +0200

    Update void-chroot-after.sh

--- void-chroot-after.sh
+++ void-chroot-after.sh
@@ -132,6 +132,9 @@ chsh -s /bin/bash $username
 ##PS1="\[\e[1;31m\][\u@\h \W]\$\[\e[m\] "
 #EOF
 
+# install rbw
+cargo install rbw
+
 # edit /etc/default/grub (set GRUB_DISTRIBUTOR)
 
 # install grub
